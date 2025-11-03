153660b929895299977f1c57e0681aef8ba740d9
muto ryoko <obng3prn3@gmail.com>
2025-11-03 23:57:26 +0900

    取引画面編集

--- src/resources/views/chat.blade.php
+++ src/resources/views/chat.blade.php
@@ -9,34 +9,54 @@
 @section('content')
     <div class="content">
         <div class="aside">
-            <h2>その他の取引</h2>
+            <p class="aside__title">その他の取引</p>
         </div>
 
         <div class="chat__wrapper">
             <div class="chat__header">
-                <h1 class="chat__title">【ユーザー名さん】との取引画面</h1>
-                <form class="chat-finished__form" action="" method="POST">
+                <div class="user-info">
+                    {{-- @if($comment->user->profile && $comment->user->profile->user_image)
+                        <div class="user-image__wrapper">
+                            <img src="{{ asset('storage/'.$comment->user->profile->user_image) }}" alt="ユーザーアイコン" class="user-image" />
+                        </div>
+                    @else --}}
+                        <div class="user-image__wrapper">
+                            <img src="{{ asset('storage/profile-images/no-image.png')}}" alt="no-image" class="user-image">
+                        </div>
+                    {{-- @endif --}}
+                    <h1 class="chat__title">【ユーザー名さん】との取引画面</h1>
+                </div>
+                <form action="" method="POST">
                     @csrf
                     <button class="chat-finished__button">取引を完了する</button>
                 </form>
             </div>
-            <div class="chat__item-info">
+            <div class="item-info">
                 <div class="item-image__wrapper">
                     <img src="" alt="商品画像">
                 </div>
-                <h2 class="item-name">商品名</h2>
-                <p class="item-price">商品価格</p>
+                <div>
+                    <h2 class="item-name">商品名</h2>
+                    <p class="item-price">
+                        <span class="yen">¥</span>
+                        商品価格
+                    </p>
+                </div>
             </div>
             <div class="chat-area">
-                <form action="" method="POST">
+
+                {{-- チャット内容 --}}
+
+                {{-- メッセージ入力欄 --}}
+                <form class="chat__form" action="" method="POST">
                     @csrf
-                    <input type="text" placeholder="取引メッセージを記入してください">
+                    <input class="chat__input" type="text" placeholder="取引メッセージを記入してください">
                     <label class="file__upload-btn">
-                        画像を選択する
+                        画像を追加
                         <input class="file-input" type="file">
                     </label>
-                    <button>
-                        <img src="{{ asset('materials/send-chat.png') }}" alt="送信">
+                    <button class="chat__submit">
+                        <img class="send-icon" src="{{ asset('materials/send-chat.png') }}" alt="送信">
                     </button>
                 </form>
             </div>
