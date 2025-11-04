7e388d5cde8e0623191aa4fae00b5b1d5789ce0a
muto ryoko <obng3prn3@gmail.com>
2025-11-05 04:58:23 +0900

    チャット機能実装

--- src/resources/views/chat.blade.php
+++ src/resources/views/chat.blade.php
@@ -51,40 +51,68 @@
                 </div>
             </div>
             <div class="chat-area">
-                <div class="chat__content">
+                <div class="chat-content">
                     @foreach ($messages as $message)
                         @if ($message->sender_id === $user->id)
                             <div class="my-message">
-                                <p class="chat__user-name">{{ $user->name }}</p>
-                                <p>{{ $message->body }}</p>
-                                <p>{{ $message->image ?? '' }}</p>
+                                <div class="chat__user-info">
+                                    <p class="chat__user-name">{{ $user->name }}</p>
+                                    @if($user->profile && $user->profile->user_image)
+                                    <div class="user-image__wrapper chat__user-image">
+                                        <img src="{{ asset('storage/'.$user->profile->user_image) }}" alt="ユーザーアイコン" class="user-image" />
+                                    </div>
+                                    @else
+                                    <div class="user-image__wrapper chat__user-image">
+                                        <img src="{{ asset('storage/profile-images/no-image.png')}}" alt="no-image" class="user-image">
+                                    </div>
+                                    @endif
+                                </div>
+                                <p class="message-body">{{ $message->body }}</p>
+                                <div>
+                                    <img class="message-image" src="{{ asset('storage/'.$message->image ?? '') }}" alt="">
+                                </div>
                             </div>
                         @else
                             <div class="partner-message">
-                                <p class="chat__user-name">{{ $partner->name }}</p>
-                                <p>{{ $message->body }}</p>
-                                <p>{{ $message->image ?? '' }}</p>
+                                <div class="chat__partner-info">
+                                    @if($partner->profile && $partner->profile->user_image)
+                                        <div class="user-image__wrapper chat__user-image">
+                                            <img src="{{ asset('storage/'.$partner->profile->user_image) }}" alt="ユーザーアイコン" class="user-image" />
+                                        </div>
+                                    @else
+                                        <div class="user-image__wrapper chat__user-image">
+                                            <img src="{{ asset('storage/profile-images/no-image.png')}}" alt="no-image" class="user-image">
+                                        </div>
+                                    @endif
+                                    <p class="chat__user-name">{{ $partner->name }}</p>
+                                </div>
+                                <p class="message-body">{{ $message->body }}</p>
+                                <div>
+                                    <img class="message-image" src="{{ asset('storage/'.$message->image ?? '') }}" alt="">
+                                </div>
                             </div>
                         @endif
                     @endforeach
                 </div>
                 {{-- メッセージ入力欄 --}}
-                <form class="chat__form" action="{{ route('chat.store', ['transaction' => $transaction->id]) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <input class="chat__input" type="text" name="body" placeholder="取引メッセージを記入してください" value="{{ old('body') }}">
-                    <label class="file__upload-btn">
-                        画像を追加
-                        <input class="file-input" name="image" type="file">
-                    </label>
-                    <button class="chat__submit" type="submit">
-                        <img class="send-icon" src="{{ asset('materials/send-chat.png') }}" alt="送信">
-                    </button>
-                </form>
-                @if($errors->hasAny(['body', 'image']))
+                <div class="chat-form__wrapper">
+                    <form class="chat-form" action="{{ route('chat.store', ['transaction' => $transaction->id]) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <input class="chat-input" type="text" name="body" placeholder="取引メッセージを記入してください" value="{{ old('body') }}">
+                        <label class="file-upload-btn">
+                            画像を追加
+                            <input class="file-input" name="image" type="file">
+                        </label>
+                        <button class="chat-submit" type="submit">
+                            <img class="send-icon" src="{{ asset('materials/send-chat.png') }}" alt="送信">
+                        </button>
+                    </form>
+                    @if($errors->hasAny(['body', 'image']))
                     <p class="error">
                         {{ $errors->first('body') ?: $errors->first('image') }}
                     </p>
-                @endif
+                    @endif
+                </div>
             </div>
         </div>
     </div>
